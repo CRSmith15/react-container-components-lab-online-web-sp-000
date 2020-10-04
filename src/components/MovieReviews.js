4a513593517b271b6f0fbe585db2b024fc817153
Chance Smith <chancesmith97@hotmail.com>
2020-10-04 21:27:26 +0000

    Automatically backed up by Learn

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,14 @@
 // Code MovieReviews Here
+import React from 'react';
+
+const Review = ({byline, headline, summary_short}) => {
+  return (
+    <div className='review'>
+      <li>
+      <h3>{headline}</h3>
+      <h4>By: {byline}</h4>
+      <p>{summary_short}</p>
+      </li>
+    </div>
+  )
+}
